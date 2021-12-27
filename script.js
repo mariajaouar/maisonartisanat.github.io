0ddbf2a3d8fafef336de036ac1d15a8879e31fd7
mariajaouar <96199505+mariajaouar@users.noreply.github.com>
2021-12-27 18:12:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ setTimeout( () => {
     cookieContainer.classList.add("active");
         
 }, 2000);
+
+      //SLIDER
 var slideIndex = 0;
 showSlides();
 
@@ -36,5 +38,15 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+       //SCROLL BAR
+
+let progressBar = document.querySelector("#progressBar");
+let totalPageHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = () => {
+    let newProgressHeight = (window.pageYOffset / totalPageHeight) * 100;
+    progressBar.style.height = `${newProgressHeight}%`;
+     progressBar.style.opacity = `${newProgressHeight}%`;
 }
